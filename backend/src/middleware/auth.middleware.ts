1808ca175dbfb983548fcf99a9cef83275d5897b
rasathelamedude <rasyar.safen19@gmail.com>
2026-06-05 18:14:14 +0300

    Update authentication middleware to get JWT tokens from cookies instead of headers

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,24 +1,18 @@
 import { Elysia } from "elysia";
-import { JWTPayload } from "../types/ApiResponse";
 import { jwtPlugin } from "../lib/jwt";
 
 export const authMiddleware = (app: Elysia) =>
-  app.use(jwtPlugin).derive(async ({ headers, jwt, set }) => {
-    const authHeader = headers.authorization;
-
-    if (!authHeader) {
-      set.status = 401;
-      throw new Error("No authorization header provided");
-    }
-
-    const token = authHeader.split(" ")[1];
+  app.use(jwtPlugin).derive(async ({ cookie, jwt, set }) => {
+    const token: string | undefined = cookie.authToken?.value as
+      | string
+      | undefined;
 
     if (!token) {
       set.status = 401;
       throw new Error("No token provided");
     }
 
-    const payload = (await jwt.verify(token)) as JWTPayload | false;
+    const payload = await jwt.verify(token);
 
     if (!payload || payload.userId === undefined) {
       set.status = 401;
